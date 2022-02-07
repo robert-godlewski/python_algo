e579b992eb6246401315612a75bd8814741db537
Robert Godlewski <robtechg5@gmail.com>
2022-02-06 21:12:35 -0800

    Have an o(n) solution for sorting squared numbers in a list.

--- arrays/array_solutions.py
+++ arrays/array_solutions.py
@@ -20,13 +20,16 @@ class Solution:
         return even_digit_nums
 
     # O(n**2) solution - need to find a better one
-    def sortedSquares(self, nums):
-        i = 0
+    def sortedSquaresNestedLoop(self, nums):
         print(f"Original list is: {nums}")
+        i = 0
         while i <len(nums):
+            print(f"index of nums we are on: {i}")
+            print(f"currently squaring {nums[i]}")
             if nums[i] != 0:
                 squ = nums[i]**2
                 nums[i] = squ
+            print(f"the squared number is {nums[i]}")
             i += 1
         print(f"Unordered list is: {nums}")
         for index in range(1, len(nums)):
@@ -39,4 +42,31 @@ class Solution:
                     i -= 1
                 else: break
         print(f"Ordered list is: {nums}")
+        return nums
+
+    # Recursive O(N) solution - Leetcode times out for some reason still
+    def sortedSquares(self, nums):
+        # print(f"current list working on is: {nums}")
+        if nums is None: return list()
+        num = nums.pop()
+        # print(f"the current number working on is: {num}")
+        if num != 0: num = num**2
+        # print(f"num squared is: {num}")
+        if len(nums) > 0: nums = self.sortedSquares(nums)
+        elif len(nums) == 0:
+            nums.append(num)
+            # print(f"first of the ordered list: {nums}")
+            return nums
+        i = 0
+        # print("-----Entering the sorting phase---------")
+        while i < len(nums):
+            # print(f"nums[i] is: {nums[i]}")
+            if nums[i] > num:
+                nums.insert(i, num)
+                break
+            elif i == len(nums)-1:
+                nums.append(num)
+                break
+            i += 1
+        # print(f"List is now: {nums}")
         return nums
