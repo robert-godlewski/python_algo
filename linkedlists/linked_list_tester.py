4569e89ec45a3c674c6e4693d5d0a69fb5f78870
Robert Godlewski <robtechg5@gmail.com>
2022-08-01 14:15:55 -0700

    added a reversed linked list solution in Aug 2022.

--- linkedlists/linked_list_tester.py
+++ linkedlists/linked_list_tester.py
@@ -1,4 +1,4 @@
-from linked_list_solutions202208 import Solution
+from linked_list_solutions202208 import ListNode, LinkedList, Solution
 
 
 solver = Solution()
@@ -6,3 +6,31 @@ solver = Solution()
 
 print("####################")
 print("Testing linked list algorithms")
+
+
+def printList(ll):
+    curr = ll.head
+    while curr:
+        print(curr.val)
+        curr = curr.next
+
+
+# Testing Reverse Linked List - Singly
+node5 = ListNode(5)
+node4 = ListNode(4, next=node5)
+node3 = ListNode(3, next=node4)
+node2 = ListNode(2, next=node3)
+node1 = ListNode(1, next=node2)
+ll1 = LinkedList(node1)
+print("-----")
+print("Reversing Singly Linked Lists")
+print("list at start:")
+printList(ll1)
+ll1.head = solver.reverseList(ll1.head)
+print("List reversed:")
+printList(ll1)
+print("empty list: []")
+emptyList = LinkedList()
+emptyList.head = solver.reverseList(emptyList.head)
+print("empty list reversed:")
+printList(emptyList)
