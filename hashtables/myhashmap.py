19baa7fbc9c878f3ffec35c545fc023702c8ff53
Robert Godlewski <robtechg5@gmail.com>
2022-09-13 15:05:11 -0700

    Attempted to resolve designing a HashMap.

--- hashtables/myhashmap.py
+++ hashtables/myhashmap.py
@@ -1,42 +1,27 @@
 # My solution on Aug 2022 - Still has runtime issues though
-# Got solution from https://medium.com/analytics-vidhya/design-a-hashmap-day-1-python-8c5e463a0629 - Doesn't work, commeted out
+# My solution - Doesn't work in leetcode for some reason
 class MyHashMap:
     def __init__(self):
         self.map_length = 10**4
         self.hash_list = [-1 for i in range(self.map_length)]
 
     def put(self, key, value):
-        self.hash_list[key] = value
-        '''
-        subkey = key % self.hash_list
-        if self.hash_list[subkey] == -1:
-            self.hash_list[subkey] = [[key, value]]
-        for i, kv in enumerate(self.hash_list[subkey]):
-            if kv[0] == key: self.hash_list[subkey][i][1] = value
-        '''
+        #self.hash_list[key] = value
+        subkey = key % self.map_length
+        self.hash_list[subkey] = value
 
     def get(self, key):
-        return self.hash_list[key]
-        '''
-        subkey = key % self.hash_list
+        #return self.hash_list[key]
+        subkey = key % self.map_length
         if self.hash_list[subkey] == -1:
             return -1
-        for k, v in self.hash_list[subkey]:
-            if k == key: return v
-        return -1
-        '''
+        else:
+            return self.hash_list[subkey]
 
     def remove(self, key):
-        self.hash_list[key] = -1
-        '''
-        subkey = key % self.hash_list
-        rem = -1
-        for i, kv in enumerate(self.hash_list[subkey]):
-            if kv[0] == key:
-                rem = i
-                break
-        if rem != -1: del self.hash_list[subkey][rem]
-        '''
+        #self.hash_list[key] = -1
+        subkey = key % self.map_length
+        self.hash_list[subkey] = -1
 
 
 # Your MyHashMap object will be instantiated and called as such:
@@ -45,6 +30,7 @@ class MyHashMap:
 # param_2 = obj.get(key)
 # obj.remove(key)
 
+'''
 myHashMap = MyHashMap()
 myHashMap.put(1, 1) # The map is now [[1,1]]
 myHashMap.put(2, 2) # The map is now [[1,1], [2,2]]
@@ -54,3 +40,4 @@ myHashMap.put(2, 1) # The map is now [[1,1], [2,1]] (i.e., update the existing v
 myHashMap.get(2)    # return 1, The map is now [[1,1], [2,1]]
 myHashMap.remove(2) # remove the mapping for 2, The map is now [[1,1]]
 myHashMap.get(2)    # return -1 (i.e., not found), The map is now [[1,1]]
+'''
