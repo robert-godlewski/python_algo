45d666aebd0963f1e3ca4f0a01cdae80e3db52a1
Robert Godlewski <robtechg5@gmail.com>
2024-10-02 16:31:32 -0700

    able to search a matrix using a binary search

--- algo/solutions/recursion2_solutions.py
+++ algo/solutions/recursion2_solutions.py
@@ -31,3 +31,35 @@ class Solution:
                 j += 1
                 n += 1
         return nums
+
+    # Leetcode Search a 2D Matrix II
+    # Time Complexity = O(nlogn)
+    # Space Complexity = O(n)
+    # Solved in 20 min
+    # Binary Search on each line of the matrix
+    def _binarySearchLine(self, matrix: list[list[int]], target: int, index: int) -> bool:
+        left = 0
+        right = len(matrix[index])-1
+        while left <= right:
+            mid = (left+right)//2
+            if matrix[index][mid] == target:
+                return True
+            elif matrix[index][mid] < target:
+                left = mid+1
+            elif matrix[index][mid] > target:
+                right = mid-1
+        return False
+
+    # Main solution
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        if len(matrix) > 0:
+            i = 0
+            while i < len(matrix):
+                if target >= matrix[i][0] and target <= matrix[i][len(matrix[i])-1]:
+                    temp = self._binarySearchLine(matrix, target, i)
+                else:
+                    temp = False
+                if temp:
+                    return True
+                i += 1
+        return False
--- algo/tests/recursion2_tester.py
+++ algo/tests/recursion2_tester.py
@@ -12,6 +12,17 @@ def recursion2Algorithms() -> None:
 
     mergeSortTest([5,2,3,1])
     mergeSortTest([5,1,1,2,0,0])
+    thinline()
+
+    matrix = [
+        [1,4,7,11,15],
+        [2,5,8,12,19],
+        [3,6,9,16,22],
+        [10,13,14,17,24],
+        [18,21,23,26,30]
+    ]
+    searchMatrixTest(matrix, 5)
+    searchMatrixTest(matrix, 20)
 
 
 # Testing sortArray - Based off of a merge sorting algorithm
@@ -19,3 +30,8 @@ def mergeSortTest(nums: list[int]) -> None:
     print(f"Sorting {nums} in order:")
     ans = solver.sortArray(nums)
     print(f"Sorted = {ans}")
+
+# Testing searchMatrix
+def searchMatrixTest(matrix: list[list[int]], target: int) -> None:
+    ans = solver.searchMatrix(matrix, target)
+    print(f"Is {target} in {matrix}? {ans}")
